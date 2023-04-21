c4af349dd1319d068e039b317712ffed88c16855
zhjxe <124144872+zhjxe@users.noreply.github.com>
2023-04-21 17:44:56 +0000

    Update server.js

--- files/server.js
+++ files/server.js
@@ -16,14 +16,14 @@ app.get("/", function (req, res) {
 });
 
 // 页面访问密码
-app.use((req, res, next) => {
-  const user = auth(req);
-  if (user && user.name === username && user.pass === password) {
-    return next();
-  }
-  res.set("WWW-Authenticate", 'Basic realm="Node"');
-  return res.status(401).send();
-});
+// app.use((req, res, next) => {
+//   const user = auth(req);
+//   if (user && user.name === username && user.pass === password) {
+//     return next();
+//   }
+//   res.set("WWW-Authenticate", 'Basic realm="Node"');
+//   return res.status(401).send();
+// });
 
 //获取系统进程表
 app.get("/status", function (req, res) {
